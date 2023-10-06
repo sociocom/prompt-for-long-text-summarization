c1929ccac0a1bdd84e5c92a113dc21cb2ffaf295
Li <LKF1013606100@outlook.com>
2023-10-06 23:29:34 +0900

    do test

---
+++ test.sh
@@ -0,0 +1,15 @@
+nohup python test.py \
+    --model_name_or_path facebook/bart-base \
+    --do_train \
+    --do_eval \
+    --dataset_name cnn_dailymail \
+    --dataset_config "3.0.0" \
+    --source_prefix "summarize: " \
+    --output_dir /tmp/tst-summarization \
+    --per_device_train_batch_size=1 \
+    --per_device_eval_batch_size=1 \
+    --overwrite_output_dir \
+    --predict_with_generate \
+    --max_train_samples 100 \
+    --max_eval_samples 50 \
+    --max_predict_samples 50 \
